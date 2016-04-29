3788662ed73eb245afcead136c6abe08dd396156
Stino <Linksonder@gmail.com>
2016-04-29 12:44:58 +0200

    Update Oauth keys

--- config/oauth.js
+++ config/oauth.js
@@ -1,8 +1,8 @@
 var host = process.env.ADVANS_HOST || "http://localhost:3000";
 
 module.exports = {
-	public: "ee033a1752e6e4b8b72c4d300f6aef95521e0e43",
-	secret: "88687cfa9e2b5a281658e75ba53e1b011671e85a",
+	public: "5943fa1089522fb407710f96d469860d517c9cee",
+	secret: "2d309566e522425b4d55d661a3ce78c50c4b1c41",
 	requestTokenURL: 'https://publicapi.avans.nl/oauth/request_token',
     accessTokenURL: 'https://publicapi.avans.nl/oauth/access_token',
     userAuthorizationURL: 'https://publicapi.avans.nl/oauth/saml.php',
